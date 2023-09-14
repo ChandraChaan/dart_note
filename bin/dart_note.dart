0f91d6ba6c732310f7dbc54be9e5a136980eb7db
Chandra <dchandrachaan@gmail.com>
2023-09-14 07:19:14 +0530

    list added

--- bin/dart_note.dart
+++ bin/dart_note.dart
@@ -1,3 +1,38 @@
-void main(){
-  print('Hello Dart');
+void main() {
+  String name = "Ramu";
+  int age = 28;
+  double height = 5.8;
+  bool handiCaped = false;
+  Map employees = {
+    "name": "Ramu",
+    "weight": 60.7,
+    "handiCaped": false,
+    "age": 30,
+    "state": "AP"
+  };
+  print(employees['state']);
+
+  List dataEmployees = [
+    // 0
+    {
+      "name": "Ramu",
+      "weight": 60.7,
+      "handiCaped": false,
+      "age": 30,
+      "state": "AP"
+    },
+    // 1
+    {
+      "name": "somu",
+      "weight": 70.7,
+      "handiCaped": true,
+      "age": 29,
+      "state": "TS"
+    }
+  ];
+//int
+  List dataCount = [7, 4, 1, 9, 6];
+  List dataTitles = ["Title 1", "Title 2"];
+// for(int a=0; a<5)
+  print(dataEmployees[1]['name']);
 }
